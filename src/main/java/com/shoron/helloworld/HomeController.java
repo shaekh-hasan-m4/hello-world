62f94a4bfe3275c12104b4349c692d103f5794be
abdullah-almomin-m4 <abdullah.almomin@metafour.com>
2018-04-06 16:03:33 +0600

    First Commit

---
+++ src/main/java/com/shoron/helloworld/HomeController.java
@@ -0,0 +1,35 @@
+package com.shoron.helloworld;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		
+		return "home";
+	}
+	
+	
+	@RequestMapping(value="/sayname")
+	@ResponseBody
+	public String sayName(){
+		return "My name is Shoron";
+	}
+	
+}
